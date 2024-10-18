fd96a368bcdfbc17eff9a43da0c80836492df26e
Taj-2005 <tajuddinshaik786k@gmail.com>
2024-10-18 09:17:02 +0530

    Delete hello-world7.cpp

--- hello-world7.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main(){
-    cout<<char(72)<<char(101)<<char(108)<<char(108)<<char(111)<<char(32);//prints "Hello "
-    cout<<char(119)<<char(111)<<char(114)<<char(108)<<char(100)<<char(33);//prints "world"
-return 0;
-}
