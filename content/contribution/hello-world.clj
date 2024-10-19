40c72fc0c7e5e4e009d2d528d92a768f0adf650e
Abhay Kumar <abhay292005@gmail.com>
2024-10-19 12:38:06 +0530

    Add Hello world in Clojure language

---
+++ content/contribution/hello-world.clj
@@ -0,0 +1,7 @@
+;Clojure is a high level, dynamic functional programming language. Clojure is designed based on the LISP programming language and has compilers which makes it run on both Java and .Net runtime environment.
+
+;Please run this code on any online compiler
+(defn hello-world []
+  (println "Hello, World!"))
+
+(hello-world)
